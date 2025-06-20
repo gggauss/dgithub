b01e1f50198b7b49978914502ee981e635dc2eea
gggauss <gaosiwei@gmail.com>
2025-06-20 18:44:10 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,9 +1,5 @@
 // 针对Cloudflare Pages的GitHub镜像 - 使用Pages Functions
 export async function onRequest(context) {
-  const request = context.request;
-  const url = new URL(request.url);
-  const path = url.pathname;
-  const search = url.search;
   if(true) {
       return new Response("Hello, world!");
   }
